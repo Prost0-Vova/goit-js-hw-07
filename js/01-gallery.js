bfdb411094cd0b3883987a014b2680e918576e46
Prost0-Vova <122817734+Prost0-Vova@users.noreply.github.com>
2023-05-21 22:47:18 +0200

    update

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,40 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const createGalleryImages = ({
+  preview,
+  original,
+  description,
+}) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+
+const galleryImagesList = galleryItems.map((el) => createGalleryImages(el));
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryImagesList.join(""));
+
+
+const onContainerClick = (e) => {
+    e.preventDefault()
+    
+    const source = e.target.dataset.source;
+    if (!source) {
+        return
+    }
+   const instance = basicLightbox.create(`
+    <img src="${source}" width="1280" height="auto">
+`);
+
+   instance.show();
+
+}
+
+galleryContainer.addEventListener("click", onContainerClick)
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,30 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector(".gallery");
+const createGalleryImages = ({
+  preview,
+  original,
+  description,
+}) => `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>`;
+
+const galleryImagesList = galleryItems.map((el) => createGalleryImages(el));
+
+galleryContainer.insertAdjacentHTML("afterbegin", galleryImagesList.join(""));
+
+const captions = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+  captionType: "alt",
+});
+
+
+
